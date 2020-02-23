ab95a7d74bccfda39f0e91c5578839b91fc95697
David Vloedman <djvloedman@gmail.com>
2020-02-23 12:11:48 -0800

    initial import

---
+++ build/precache-manifest.9904be2cbbeb6e5188c8f4011d69fcb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57862c7c2685655733a895fc9f408d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd4a4324027cac85241",
+    "url": "/static/css/main.28b600d2.chunk.css"
+  },
+  {
+    "revision": "480bac16ebdd341ab377",
+    "url": "/static/js/2.f2e2b20f.chunk.js"
+  },
+  {
+    "revision": "ddd4a4324027cac85241",
+    "url": "/static/js/main.51174b72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/jsonText/index.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+
+
+/**
+ * JsonText class/component
+ * @description text area with crucial behaviour being passed down via props
+ */
+
+
+
+class JsonText extends React.Component {
+  render() {
+    return (
+      <textarea
+        placeholder="Raw JSON Body"
+        name="requestBody"
+        onChange={this.props.onChange}
+        value={this.props.value}
+        disabled={          
+          this.props.disabled
+        }
+      />
+    );
+  }
+}
+
+
+export default JsonText;
